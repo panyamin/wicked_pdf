2ceb9e1a2b6da50703e8ef158631552ad4c6acb5
Benjamin <panyamin@users.noreply.github.com>
2015-03-25 12:17:59 +0100

    Update tempfile.rb

--- lib/wicked_pdf/tempfile.rb
+++ lib/wicked_pdf/tempfile.rb
@@ -3,7 +3,8 @@ require 'tempfile'
 class WickedPdfTempfile < Tempfile
   # ensures the Tempfile's filename always keeps its extension
   def initialize(filename, temp_dir = nil)
-    temp_dir  ||= Dir.tmpdir
+    # temp_dir  ||= Dir.tmpdir
+    temp_dir = `cygpath -dm /tmp`.strip
     extension = File.extname(filename)
     basename  = File.basename(filename, extension)
     super([basename, extension], temp_dir)
